c8b462a5897d1831f66d0b7fd5e8c62dd22da4ff
권한준 <skclaqks11@naver.com>
2016-10-13 20:33:23 +0900

    메인 함수

--- homework4/src/homework4/ExamForWhile.java
+++ homework4/src/homework4/ExamForWhile.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class ExamForWhile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
 		while (true) {
@@ -12,8 +11,8 @@ public class ExamForWhile {
 			System.out.println("3. 입력받은 숫자의 합곙화 평균구하기");
 			System.out.println("4. 원하는 구구단 출력하기");
 			System.out.println("5. 짝수단/홀수단 출력하기");
-			System.out.println("6. 종요하기");
-			System.out.println("원하는 메뉴는 >>");
+			System.out.println("6. 종료하기");
+			System.out.print("원하는 메뉴는 >>");
 			int select = sc.nextInt();
 
 			if (select == 1) {
