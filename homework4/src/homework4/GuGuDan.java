32d11a9f9209a8b6bb619e4115a9d1f2fa012fc2
권한준 <skclaqks11@naver.com>
2016-10-13 20:34:22 +0900

    원하는 구구단 출력하기

--- homework4/src/homework4/GuGuDan.java
+++ homework4/src/homework4/GuGuDan.java
@@ -2,28 +2,30 @@ package homework4;
 import java.util.Scanner;
 
 public class GuGuDan {
-	int dan=0;
+	String dan;
 	GuGuDan(){}
 	
 	void guguCalc(){
-		System.out.println("##우너하는 구구단 출력하기");
+		System.out.println("###원하는 구구단 출력하기");
 		Scanner sc = new Scanner(System.in);
 		while(true){
 			System.out.print("출력하고 싶은 단(Q:종료) >> ");
-			dan = sc.nextInt();
+			dan = sc.nextLine();
 			
-			if(dan>1 && dan<10){
+			if(dan.equals("Q"))
+				break;
+			else if(Integer.parseInt(dan)>1 && Integer.parseInt(dan)<10){
 				for(int i=1; i<10; ++i){
 					if(i%2==0)
-						System.out.printf("%d * %d = %d\n",dan,i,dan*i);
+						System.out.printf("%d * %d = %d\n",Integer.parseInt(dan),i,Integer.parseInt(dan)*i);
 					else
-						System.out.printf("%d * %d = %d     ",dan,i,dan*i);
+						System.out.printf("%d * %d = %d     ",Integer.parseInt(dan),i,Integer.parseInt(dan)*i);
 				}
+				System.out.println();
 			}
-			else if(dan=='Q')
-				break;
 			else
 				System.out.println("===>잘 못 입력");		
+			
 		}
 	}
 
