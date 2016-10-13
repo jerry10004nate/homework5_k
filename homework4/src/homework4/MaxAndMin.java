03bb6b7a1a1cbbd164e6d8a62eea87e739c55c92
권한준 <skclaqks11@naver.com>
2016-10-13 20:35:02 +0900

    1~100까지 가장 큰수/작은 수 구하기

--- homework4/src/homework4/MaxAndMin.java
+++ homework4/src/homework4/MaxAndMin.java
@@ -10,23 +10,24 @@ public class MaxAndMin {
 	void MaxMin(){
 		System.out.println("###최대값/최소값구하기");
 	
-		int i;
+		String i;
 		Scanner sc = new Scanner(System.in);
 		
 		while(true){
 			System.out.print("숫자를 입력(Q:종료) >> ");
-			i = sc.nextInt();
-			if(i=='Q')
+			i = sc.nextLine();
+			
+			if(i.equals("Q"))
 				break;
-			else if(i>=0 && i<=100){
-				if(i>max)
-					max=i;
-				else if(i<min)
-					min=i;
+			else if(Integer.parseInt(i)>=0 && Integer.parseInt(i)<=100){
+				if(Integer.parseInt(i)>max)
+					max=Integer.parseInt(i);
+				else if(Integer.parseInt(i)<min)
+					min=Integer.parseInt(i);
 			}
 			else continue;
 		}
-		System.out.printf("가장 큰 수는 %d이고, 작은수는 %d입니다.",max, min);
+		System.out.printf("가장 큰 수는 %d이고, 작은수는 %d입니다.\n",max, min);
 		return;
 	}
 }
